1488af602516dec54d3a8fe9dedef6b92064bfdd
AStruthers2000 <struthers919@gmail.com>
2023-10-13 09:34:54 -0700

    Set up basic inhanced input

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,32 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Will halt execution if this condition is false (i.e. AuraContext == nullptr)
+	check(AuraContext);
+	auto *Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	//add the mapping context to the input subsystem
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	//don't hide cursor
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	//configure mouse input behavior
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
