c17e912b419b0e7b8488ffe637742b27596b9a56
AStruthers2000 <struthers919@gmail.com>
2023-10-13 09:11:23 -0700

    Created basic animation blueprints for Aura and goblins

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -17,5 +17,13 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	/*
+	 * TObjectPtr is relatively new. In packaged build, it is the sam as a raw pointer.
+	 * But in editor, it has access tracking and optional lazy load features.
+	 * Access tracking lets us know how much a pointer is accessed and how often.
+	 * Lazy loading will keep the memory unload until it is really needed
+	 */
+	UPROPERTY(EditAnywhere, Category="Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 };
