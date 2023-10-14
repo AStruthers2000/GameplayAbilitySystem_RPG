b30b96231f5a96544eb30334ffbb6ad541eef842
AStruthers2000 <struthers919@gmail.com>
2023-10-14 13:27:00 -0700

    Added a BAD EffectActor, definitely not perminant solution

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,54 @@
+
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+
+	SetRootComponent(Mesh);
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::StartOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
+}
+
+void AAuraEffectActor::StartOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	/**
+	 * This is a bad way of changing attributes, we just don't know about Gameplay Effects yet
+	 * TODO: Change this whole thing to apply a GameplayEffect instead. For now, using const_cast as a hack!!!!!!
+	 */
+	if(const IAbilitySystemInterface *ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UAuraAttributeSet *AuraAttributes = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+
+		//this is a HUGE no-no, because we are NOT keeping this
+		UAuraAttributeSet *MutableAuraAttributes = const_cast<UAuraAttributeSet*>(AuraAttributes);
+		MutableAuraAttributes->SetHealth(AuraAttributes->GetHealth() + 50.f);
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	
+}
